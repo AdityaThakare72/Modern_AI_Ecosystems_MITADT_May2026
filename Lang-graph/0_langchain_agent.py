11c87ec244b510dd2bc92544f36c053045887146
Aditya Thakare <adityathakare72@gmail.com>
2026-05-14 12:56:09 +0530

    edits

--- Lang-graph/0_langchain_agent.py
+++ Lang-graph/0_langchain_agent.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
-from langchain import hub
-from langchain.agents import AgentExecutor, create_react_agent
+# from langchain import hub
+from langchain_classic.agents import AgentExecutor,create_react_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -12,12 +12,17 @@ tools = [search_tool]
 
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-pro", 
+    model="gemini-2.5-flash-lite", 
     temperature=0 # Keep it at 0 for analytical tasks so it doesn't hallucinate tools
 )
 
-# 4. Pull the ReAct Prompt Blueprint
-prompt = hub.pull("hwchase17/react")
+"""# 4. Pull the ReAct Prompt Blueprint
+prompt = hub.pull("hwchase17/react")""";
+
+from langsmith import Client
+
+client = Client()
+prompt = client.pull_prompt("hwchase17/react")
 
 # 5. Construct the ReAct Agent
 agent = create_react_agent(llm, tools, prompt)
